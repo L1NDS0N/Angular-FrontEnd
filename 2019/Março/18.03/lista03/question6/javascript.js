880457a389323e28f6f72177eed4b9c15063659f
Lindson França <l1nds0n@hotmail.com>
2019-03-19 15:01:28 -0300

    Ver novamente

--- 2019/Março/18.03/lista03/question6/javascript.js
+++ 2019/Março/18.03/lista03/question6/javascript.js
@@ -1,8 +1,8 @@
 // 6. Faça um Algoritmo que leia três números e mostre o maior deles
 alert("Número maior")
-num = [0]prompt("Insira o primeiro número:")
-num = [1]prompt("Insira o segundo número:")
-num = [2]prompt("Insira o terceiro número:")
+// num = [0]prompt("Insira o primeiro número:")
+// num = [1]prompt("Insira o segundo número:")
+// num = [2]prompt("Insira o terceiro número:")
 // let maior = 0;
 // let contador = 0;
 // for (var i = 0; i < num.length; i++) {
@@ -13,3 +13,14 @@ num = [2]prompt("Insira o terceiro número:")
 //     alert("O maior número é o: "+maior+" no índice: "+contador);
 //   }
 // }
+let n1 = parseInt(prompt("Insira o primeiro número"))
+let n2 = parseInt(prompt("Insira o segundo número"))
+let n3 = parseInt(prompt("Insira o terceiro número"))
+
+if(n1 > n2){
+  alert("O maior é o primeiro: "+n1)
+} else if (n2 > n3) {
+  alert("O maior é o segundo: "+n2)
+} else if (n3 > n1) {
+  alert("O maior é o terceiro: "+n3)
+}
