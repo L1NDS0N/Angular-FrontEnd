312467e39db6bec6dc4f8a38509c9e7a32ee2ef2
Lindson França <l1nds0n@hotmail.com>
2019-03-19 16:56:27 -0300

    Tarefas 19.03

--- 2019/Março/18.03/lista03/question10/javascript.js
+++ 2019/Março/18.03/lista03/question10/javascript.js
@@ -2,7 +2,7 @@
 // ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou
 // "Boa Noite!" ou "Valor Inválido!", conforme o caso
 alert("Qual o turno?")
-let turno = prompt("Insira em que turno você estuda:")
+let turno = prompt("Insira em que turno você estuda:").toLowerCase()
 switch (turno) {
   case "m":
   alert("Bom dia!")
@@ -10,7 +10,7 @@ switch (turno) {
   case "v":
   alert("Boa tarde!")
   break;
-  case "n": case "noturno".toLocaleLowerCase():
+  case "n": case "noturno":
   alert("Boa noite!")
   break;
   default:
