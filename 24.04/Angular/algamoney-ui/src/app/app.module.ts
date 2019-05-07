376a69abf22f39c7b09a18a73d2f8967fbe3eab5
Lindson França <l1nds0n@hotmail.com>
2019-05-07 16:47:15 -0300

    Adicionando calendário no lançamento

--- 24.04/Angular/algamoney-ui/src/app/app.module.ts
+++ 24.04/Angular/algamoney-ui/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { TooltipModule } from 'primeng/components/tooltip/tooltip'
-import { TabViewModule } from 'primeng/primeng';
+import { TabViewModule } from 'primeng/components/tabview/tabview';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { ButtonModule } from 'primeng/components/button/button';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
@@ -10,7 +11,6 @@ import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-
 
 @NgModule({
   declarations: [
@@ -25,7 +25,8 @@ import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.co
     InputTextModule,
     ButtonModule,
     DataTableModule,
-    TooltipModule
+    TooltipModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 24.04/Angular/algamoney-ui/src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ 24.04/Angular/algamoney-ui/src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -49,13 +49,19 @@ export class PessoasPesquisaComponent {
       cidade: 'Mensalidade musculação',
       status: false,
       nome: 'Academia Top'
-    }
+    },
+    { nome: 'Manoel Pinheiro', cidade: 'Uberlândia', estado: 'MG', status: true },
+    { nome: 'Sebastião da Silva', cidade: 'São Paulo', estado: 'SP', status: false },
+    { nome: 'Carla Souza', cidade: 'Florianópolis', estado: 'SC', status: true },
+    { nome: 'Luís Pereira', cidade: 'Curitiba', estado: 'PR', status: true },
+    { nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro', estado: 'RJ', status: false },
+    { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', status: true }
   ];
-  
-  // checkstatus(){
-  //   if(this.pessoas.values.status == true){
-  //     let checkedstatus: string = "Ativo";
+
+  // checkstatus(status){
+  //   if(this.pessoas.status == true){
+  //
   //   }
-  //    checkedstatus: string = "Inativo";
+  //
   // }
 }
