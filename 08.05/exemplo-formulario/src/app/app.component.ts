1e229c67def1658f98c9791c659783c771ba9355
Lindson França <l1nds0n@hotmail.com>
2019-05-08 16:30:53 -0300

    Tratando excessões com template driven

---
+++ 08.05/exemplo-formulario/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+
+
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+
+}
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  cliente = new Cliente();
+  title = 'app';
+
+  profissoes = ['Programador', 'Empresário', 'Outra'];
+
+  salvar(form: NgForm){
+
+  }
+}
