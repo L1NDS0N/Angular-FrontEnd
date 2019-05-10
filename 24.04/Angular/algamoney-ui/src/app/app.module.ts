c0d5d82aff528fd7fcd0e5ea2a1d2159137f37be
Lindson França <l1nds0n@hotmail.com>
2019-05-10 16:36:50 -0300

    Formulários completos... Implementar as modulações!

--- 24.04/Angular/algamoney-ui/src/app/app.module.ts
+++ 24.04/Angular/algamoney-ui/src/app/app.module.ts
@@ -1,37 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { TooltipModule } from 'primeng/components/tooltip/tooltip'
 import { TabViewModule } from 'primeng/components/tabview/tabview';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
 import { CalendarModule } from 'primeng/components/calendar/calendar';
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { ButtonModule } from 'primeng/components/button/button';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
-import { InputTextAreaModule } from 'primeng/components/inputtextarea/inputtextarea';
+import {InputTextareaModule} from 'primeng/components/inputtextarea/inputtextarea';
+import {InputMaskModule} from 'primeng/components/inputmask/inputmask';
+import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
+
+import { CurrencyMaskModule } from "ng2-currency-mask";
 
 import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { PessoasCadastroComponent } from './pessoas-cadastro/pessoas-cadastro.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LancamentoCadastroComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
+    PessoasCadastroComponent,
     PessoasPesquisaComponent,
-    LancamentoCadastroComponent
+    MessageComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    TabViewModule,
+    FormsModule,
+
+    CurrencyMaskModule,
     InputTextModule,
+    TabViewModule,
     ButtonModule,
     DataTableModule,
     TooltipModule,
     CalendarModule,
-    InputTextAreaModule
+    InputTextareaModule,
+    InputMaskModule,
+    SelectButtonModule,
+    DropdownModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
